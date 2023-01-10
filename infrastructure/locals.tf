d0d68d78462499e46131224e83dfc28a711c56a8
EC2 Default User <ec2-user@ip-172-31-21-165.eu-central-1.compute.internal>
2023-01-10 16:31:42 +0000

    added terraform files

---
+++ infrastructure/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  name          = "flask-final-project"
+  vpc_id        = "vpc-0b1278830a75a1458"
+  subnet_id     = "subnet-05239f092bf247d6f"
+  instance_type = "t2.large"
+  my_ip         = "0.0.0.0/0"
+}
